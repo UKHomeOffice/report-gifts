ef5d95e5d8f6d51abbc58c3d379dff90c4e07a77
cahildm <cahil.de_menezes@outlook.com>
2022-09-19 22:24:56 +0100

    content review changes v1.2

--- app/routes.js
+++ app/routes.js
@@ -4,22 +4,24 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 ///// NAVIGATION ROUTES START /////
-router.post("/received-or-offered-redirect", (req, res) => {
-  let giftReceivedOrOffered = req.session.data["received-or-offered"]
-  if (giftReceivedOrOffered == undefined) {
-    res.redirect("gifts/received-or-offered-error")
+router.post("/gift-on-someones-behalf-redirect", (req, res) => {
+  let onSomeonesBehalf = req.session.data["on-someones-behalf"]
+  if (onSomeonesBehalf == undefined) {
+    res.redirect("gifts/gift-on-someones-behalf-error")
   } else {
-    res.redirect("gifts/individual-gift")
+    res.redirect("gifts/reporting")
   }
 })
 
-//CDM: Skip on-behalf-of-gift.html
-router.post("/individual-gift-redirect", (req, res) => {
-  let individualGift = req.session.data["individual-gift"]
-  if (individualGift == "yes") {
+router.post("/reporting-redirect", (req, res) => {
+  let reporting = req.session.data["reporting"]
+  let onSomeonesBehalf = req.session.data["on-someones-behalf"]
+  if (reporting == undefined) {
+    res.redirect("gifts/reporting-error")
+  } else if (reporting != undefined && onSomeonesBehalf == "yes") {
+    res.redirect("gifts/on-behalf-of")
+  } else if (reporting != undefined && onSomeonesBehalf == "no") {
     res.redirect("gifts/other-party-details")
-  } else {
-    res.redirect("gifts/on-behalf-of-gift")
   }
 })
 
@@ -36,7 +38,10 @@ router.post("/on-behalf-of-redirect", (req, res) => {
 router.post("/employee-lookup-redirect", (req, res) => {
   let employeeLookup = req.session.data["employee-lookup"]
   let employeeName = req.session.data["employee-name"]
-  if (employeeLookup == "" && employeeName == "") {
+  let employeeEmail = req.session.data["employee-email"]
+  let employeeCCC = req.session.data["employee-cost-centre-code"]
+
+  if (employeeLookup == "" && (employeeName == "" || employeeEmail == "" || employeeCCC == "")) {
     res.redirect("gifts/employee-lookup-error")
   } else {
     res.redirect("gifts/other-party-details")
@@ -56,46 +61,40 @@ router.post("/someone-else-details-redirect", (req, res) => {
 })
 
 router.post("/gift-details-redirect", (req, res) => {
-  let giftActionReported = req.session.data["received-or-offered"]
+  let actionReported = req.session.data["reporting"]
   let giftDate = req.session.data["date-received-or-offered"]
   let giftReason = req.session.data['reason-for-gift']
   let giftCost = req.session.data['cost-of-gift']
+  let onSomeonesBehalf = req.session.data["on-someones-behalf"]
 
   if (giftDate == "" && giftReason == "" && giftCost == "") {
     res.redirect("gifts/gift-details-error")
   }
 
-  if (giftActionReported == "received") {
-    res.redirect("gifts/gift-decision")
+  if ((actionReported == "gift-received-and-accepted" || actionReported == "gift-received-and-rejected") && onSomeonesBehalf == "no") {
+    res.redirect("gifts/summary")
+  } else if ((actionReported == "gift-received-and-accepted" || actionReported == "gift-received-and-rejected") && onSomeonesBehalf == "yes") {
+    res.redirect("gifts/delegated-authority-approval")
   } else {
     res.redirect("gifts/approved-supplier")
-  }
-})
-
-router.post("/gift-decision-redirect", (req, res) => {
-  let individualGift = req.session.data["individual-gift"]
-  if (individualGift == "yes") {
-    res.redirect("gifts/summary")
-  } else {
-    res.redirect("gifts/delegated-authority-approval")
   }
 })
 
 //CDM: Skip non-approved-supplier-used-reason.html when an approved supplier is used
 router.post("/approved-supplier-redirect", (req, res) => {
   let approvedSupplierUsed = req.session.data["approved-supplier-used"]
-  let individualGift = req.session.data["individual-gift"]
+  let onSomeonesBehalf = req.session.data["on-someones-behalf"]
   let approverSupplierNotUsedReason = req.session.data["approved-supplier-not-used-reason"]
 
-  if (approvedSupplierUsed == "yes" && individualGift == "no") {
+  if (approvedSupplierUsed == "yes" && onSomeonesBehalf == "yes") {
     res.redirect("gifts/delegated-authority-approval")
-  } else if (approvedSupplierUsed == "yes" && individualGift == "yes") {
+  } else if (approvedSupplierUsed == "yes" && onSomeonesBehalf == "no") {
     res.redirect("gifts/summary")
   } else if (approvedSupplierUsed == "no" && approverSupplierNotUsedReason == "") {
     res.redirect("gifts/approved-supplier-reason-error")
-  } else if (approvedSupplierUsed == "no" && individualGift == "no" && approverSupplierNotUsedReason != "") {
+  } else if (approvedSupplierUsed == "no" && onSomeonesBehalf == "yes" && approverSupplierNotUsedReason != "") {
     res.redirect("gifts/delegated-authority-approval")
-  } else if (approvedSupplierUsed == "no" && individualGift == "yes" && approverSupplierNotUsedReason != "") {
+  } else if (approvedSupplierUsed == "no" && onSomeonesBehalf == "no" && approverSupplierNotUsedReason != "") {
     res.redirect("gifts/summary")
   }
 })
